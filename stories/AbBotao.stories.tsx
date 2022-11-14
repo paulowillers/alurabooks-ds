8af164f401121f6a7a97b26a2f32bef6c3e3f33f
Paulo Willers <paullera@gmail.com>
2022-11-14 13:59:51 -0300

    adicionando variantes do AbBotao e componente Card

--- stories/AbBotao.stories.tsx
+++ stories/AbBotao.stories.tsx
@@ -1,6 +1,6 @@
 import React from "react";
 import {ComponentMeta, ComponentStory} from "@storybook/react";
-import {AbBotao} from '../src/componentes/AbBotao';
+import {AbBotao, AbBotaoProps} from '../src/componentes/AbBotao';
 
 export default {
     title: 'Componentes/AbBotao',
@@ -9,6 +9,18 @@ export default {
 
 } as ComponentMeta<typeof AbBotao>
 
-const Template: ComponentStory<typeof AbBotao> = () => <AbBotao />
+const Template: ComponentStory<typeof AbBotao> = (args) => <AbBotao {...args}/>
 
 export const Primario = Template.bind({})
+
+Primario.args = {
+    texto: 'Ab Botão Primário',
+    tipo: 'primario'
+} as AbBotaoProps
+
+export const Secundario = Template.bind({})
+
+Secundario.args = {
+    texto: 'Ab Botão Secundário',
+    tipo: 'secundario'
+} as AbBotaoProps
