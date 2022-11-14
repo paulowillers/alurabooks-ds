3eae2db41f0f64b4a05cae8ccd82dbd45e970295
Paulo Willers <paullera@gmail.com>
2022-11-14 11:25:26 -0300

    iniciando o projeto de designt system

---
+++ src/componentes/AbBotao/index.tsx
@@ -0,0 +1,25 @@
+import React from 'react';
+import styled from 'styled-components';
+//import './AbBotao.css';
+
+const BotaoEstilizado = styled.button`
+    background: #EB9B00;
+    padding: 16px 32px;
+    border: 2px solid #EB9B00;
+    font-size: 20px;
+    color: #FFFFFF;
+    cursor: pointer;
+    &:hover {
+        background: #B87900;
+        border: 2px solid #B87900;
+    }
+`
+
+export const AbBotao = () => {
+
+    return (
+        <BotaoEstilizado>
+            Clique aqui!
+        </BotaoEstilizado>
+    );
+}
---
+++ stories/AbBotao.stories.tsx
@@ -0,0 +1,14 @@
+import React from "react";
+import {ComponentMeta, ComponentStory} from "@storybook/react";
+import {AbBotao} from '../src/componentes/AbBotao';
+
+export default {
+    title: 'Componentes/AbBotao',
+    component: AbBotao
+
+
+} as ComponentMeta<typeof AbBotao>
+
+const Template: ComponentStory<typeof AbBotao> = () => <AbBotao />
+
+export const Primario = Template.bind({})
